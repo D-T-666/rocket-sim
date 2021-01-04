581976bf2f05b8184099f34e1d353e136fee0860
Dimitri Tabatadze <tabatadzedima20@gmail.com>
2021-01-04 11:07:24 +0400

    basic functionality

--- engine.py
+++ engine.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Rocket():
     def __init__(self, mass, alt, fuel, thrust, vel, throttle):
         self.mass = mass
@@ -12,29 +13,22 @@ class Rocket():
         self.data_keys = ['mass', 'alt', 'fuel', 'thrust', 'vel', 'acc']
 
     def update(self, time_step=1):
-        acc = (self.mass - self.thrust * self.throttle)
-        self.vel -= acc/time_step
-        self.alt += self.vel/time_step
+        acc = -self.thrust * self.throttle / self.mass + 9.8
+        self.vel -= acc / time_step
+        self.alt += self.vel / time_step
 
-        return { "alt": self.alt, "vel": self.vel, "acc": acc }
+        return {"alt": self.alt, "vel": self.vel, "acc": acc}
 
 
 def run():
     data = {}
-    r = Rocket(
-        mass=100,
-        alt=100,
-        fuel=10,
-        thrust=110,
-        vel=0,
-        throttle=0
-    )
+    r = Rocket(mass=100, alt=100, fuel=10, thrust=1500, vel=0, throttle=0)
 
     for key in r.data_keys:
         data[key] = []
 
-    for i in range(0, 100, 1):
-        for k,v in r.update(time_step=20).items():
+    for i in range(0, 150, 1):
+        for k, v in r.update(time_step=20).items():
             data[k].append(v)
 
         if i > 50:
