d980c236be56cb69bed6e5873c2fc56855aecb4b
Dimitri Tabatadze <tabatadzedima20@gmail.com>
2021-01-05 23:20:46 +0400

    minor

--- engine.py
+++ engine.py
@@ -4,120 +4,134 @@ from functions import *
 
 
 class Rocket():
-    def __init__(self, mass, alt, fuel, thrust, fuel_consumption, vel, state,
-                 target_alt):
-        self.mass = mass
-        self.alt = alt
-        self.fuel = fuel
-        self.thrust = thrust
-        self.vel = vel
-        self.acc = 0
-        self.throttle = 0
-        self.fuel_consumption = 10
-        self.state = state
-        self.target_alt = target_alt
-        self.pid = {'p': 0, 'i': 0, 'd': 0}
-        self.a = 0
-        self.b = 0
-        self.c = 0
+	def __init__(self, mass, alt, fuel, thrust, fuel_consumption, vel, state,
+				 target_alt):
+		self.mass = mass
+		self.alt = alt
+		self.fuel = fuel
+		self.thrust = thrust
+		self.vel = vel
+		self.acc = 0
+		self.throttle = 0
+		self.fuel_consumption = 10
+		self.state = state
+		self.target_alt = target_alt
+		self.pid = {'p': 0, 'i': 0, 'd': 0}
+		self.a = 0
+		self.b = 0
+		self.c = 0
 
-        self.data_keys = [
-            'mass', 'alt', 'fuel', 'thrust', 'vel', 'acc', 'throttle', 'ed',
-            'fire_alt', 'a', 'b', 'c'
-        ]
+		self.data_keys = [
+			'mass', 'alt', 'fuel', 'thrust', 'vel', 'acc', 'throttle', 'ed1', 'ed2',
+			'fire_alt', 'a', 'b', 'c'
+		]
 
-    def update(self, time_steps=1):
-        self._physics(time_steps)
+	def update(self, time_steps=1):
+		self._physics(time_steps)
 
-        self._guidance(time_steps)
+		self._guidance(time_steps)
 
-        return {
-            'alt': self.alt,
-            'vel': self.vel,
-            'acc': -self.acc,
-            'throttle': self.throttle,
-            'fuel': self.fuel,
-            'ed': self.estimated_distance(),
-            'a': self.a, 
-            'b': self.b,
-            'c': self.c
-        }
+		return {
+			'alt': self.alt,
+			'vel': self.vel,
+			'acc': -self.acc,
+			'throttle': self.throttle,
+			'fuel': self.fuel,
+			'ed1': self.estimated_distance()[0],
+			'ed2': self.estimated_distance()[1],
+			'a': self.a, 
+			'b': self.b,
+			'c': self.c
+		}
 
-    def _physics(self, time_steps):
-        if self.alt <= 0:
-            return True
+	def _physics(self, time_steps):
+		if self.alt <= 0:
+			return True
 
-        self.acc = -self.thrust * (self.throttle
-                                   if self.fuel >= 1 else self.throttle *
-                                   self.fuel) / (self.mass + self.fuel) + 9.8
+		self.acc = -self.thrust * (self.throttle
+								   if self.fuel >= 1 else self.throttle *
+								   self.fuel) / (self.mass + self.fuel) + 9.8
 
-        self.vel -= self.acc / time_steps
-        self.alt += self.vel / time_steps
+		self.vel -= self.acc / time_steps
+		self.alt += self.vel / time_steps
 
-        if self.fuel * time_steps >= self.throttle:
-            self.fuel -= self.throttle * self.fuel_consumption / time_steps
+		if self.fuel * time_steps >= self.throttle:
+			self.fuel -= self.throttle * self.fuel_consumption / time_steps
+			if self.fuel < 0:
+				self.fuel = 0
 
-    def _guidance(self, time_steps):
-        self.pid['p'] = 0.6 * (self.target_alt - self.alt)
-        self.pid['i'] += 0.05 * self.vel / time_steps
-        self.pid['d'] = -2 * self.vel
+	def _guidance(self, time_steps):
+		self.pid['p'] = 0.4 * (self.target_alt - self.alt)
+		self.pid['i'] += 0.02 * (self.vel) / time_steps
+		self.pid['d'] = -2 * self.vel
 
-        if self.state == 'landing':
-            if self.estimated_distance() > self.alt+1:
-                self.throttle = 1
-                self.state = 'out of control'
-            else:
-                self.throttle = 0
+		if self.state == 'landing':
+			if self.estimated_distance()[0] > self.alt+1:
+				self.throttle = 1
+				self.state = 'out of control'
+			else:
+				self.throttle = 0
 
-        elif self.state == 'hover':
-            self.throttle = (self.pid['p'] + self.pid['i'] + self.pid['d']) \
-                            / (self.thrust / (self.mass + self.fuel))
-            if self.throttle > 1:
-                self.throttle = 1
-            elif self.throttle < 0:
-                self.throttle = 0 
+		elif self.state == 'hover':
+			self.throttle = (9.8 + self.pid['p'] + self.pid['i'] + self.pid['d']) \
+							/ (self.thrust / (self.mass + self.fuel))
+			if self.throttle > 1:
+				self.throttle = 1
+			elif self.throttle < 0:
+				self.throttle = 0 
 
-            if abs(self.vel) < 0.01 and self.alt > 110:
-                self.state = 'landing'
+			if abs(self.vel) < 0.01 and self.alt > 110:
+				# self.state = 'landing'
+				# self.target_alt = 0
+				pass
 
-        elif self.state == 'launch':
-            pass
+		# if self.vel > 10:
+		# 	self.throttle = 9.79/(self.thrust / (self.mass + self.fuel))
 
-    def estimated_distance(self):
-        # mass = self.mass + self.fuel
-        # net_acc = self.thrust / mass - 9.8
-        # time_to_stop = self.vel / net_acc
-        # time_to_stop = time_to_stop if time_to_stop < self.fuel else self.fuel
-        # estimated_distance = self.vel * time_to_stop / 2 - self.vel / 2
-        # return estimated_distance
+	def estimated_distance(self):
+		x0 = -9.8 + self.thrust / (self.mass + self.fuel - self.fuel_consumption * 0.0)
+		x1 = -9.8 + self.thrust / (self.mass + self.fuel - self.fuel_consumption * 0.01)
+		x2 = -9.8 + self.thrust / (self.mass + self.fuel - self.fuel_consumption * 1.0)
+		x3 = -9.8 + self.thrust / (self.mass + self.fuel - self.fuel_consumption * 1.01)
 
-        # ===
+		# Derivative at x=0 and x=1
+		u = -x1/x0
+		v = -x3/x2
+		# Initial height at x=0
+		y = abs(self.vel)
 
-        # Derivative at x=0 and x=1
-        u = -self.thrust / (self.mass + self.fuel) - 9.8
-        v = -self.thrust / (self.mass + self.fuel - 1) - 9.8
-        # Initial height at x=0
-        y = self.thrust / (self.mass + self.fuel) - 9.8
+		# print(u, v, y)
 
-        return get_positive_area(u, v, y)
+		# ===
+		
+		mass = self.mass + self.fuel
+		net_acc = self.thrust / mass - 9.8
+		time_to_stop = self.vel / net_acc
+		time_to_stop = time_to_stop if time_to_stop < self.fuel else self.fuel
+		estimated_distance = self.vel * time_to_stop / 2 - self.vel / 2
+		
+		return [get_positive_area(u, v, y), estimated_distance]
 
 
 def run():
-    data = {}
-    r = Rocket(mass=100,
-               alt=1,
-               fuel=250,
-               thrust=4500,
-               fuel_consumption=10,
-               vel=0,
-               state='hover',
-               target_alt=120)
+	data = {}
+	r = Rocket(mass=100,
+			   alt=1,
+			   fuel=250,
+			   thrust=4500,
+			   fuel_consumption=10,
+			   vel=0,
+			   state='hover',
+			   target_alt=1000)
 
-    for key in r.data_keys:
-        data[key] = []
+	for key in r.data_keys:
+		data[key] = []
 
-    for i in range(0, 300, 1):
-        for k, v in r.update(time_steps=5).items():
-            data[k].append(v)
+	for i in range(0, 600, 1):
+		for k, v in r.update(time_steps=10).items():
+			data[k].append(v)
 
-    return data
+		if i == 300:
+			r.state = 'landing'
+
+	return data
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import engine
+from functions import *
 
 
 def main():
@@ -10,9 +11,13 @@ def main():
     fig, axs = plt.subplots(2, 3, constrained_layout=True)
 
     axs[0][0].plot(results["alt"])
+    axs[0][0].plot([a-e for a,e in zip(results["alt"], results["ed1"])], '--', alpha=0.3)
+    # axs[0][0].plot([a-e for a,e in zip(results["alt"], results["ed2"])], '--', alpha=0.3)
+    axs[0][0].plot([0 for a in results["alt"]], '-.')
     axs[0][0].set_title('altitude')
 
     axs[0][1].plot(results["vel"])
+    axs[0][1].plot([0 for a in results["alt"]], '-.')
     axs[0][1].set_title('velocity')
 
     axs[0][2].plot(results["fuel"])
@@ -22,13 +27,16 @@ def main():
     axs[1][0].set_title('throttle')
 
     axs[1][1].plot(results["acc"])
+    axs[1][1].plot([0 for a in results["alt"]], alpha=0.5)
     axs[1][1].set_title('acceleration')
 
-    axs[1][2].plot(results["ed"])
+    axs[1][2].plot(results["ed1"])
+    # axs[1][2].plot(results["ed2"])
     axs[1][2].set_title('estimated distance')
 
     plt.show(block=True)
 
+main()
 
 # def plot():
 #     plt.style.use('dark_background')
@@ -38,30 +46,4 @@ def main():
 	
 #     plt.show(block=True)
 
-
-main()
-
 # plot()
-
-# def f(t):
-#     s1 = np.cos(2*np.pi*t)
-#     e1 = np.exp(-t)
-#     return s1 * e1
-
-# t1 = np.arange(0.0, 5.0, 0.1)
-# t2 = np.arange(0.0, 5.0, 0.02)
-# t3 = np.arange(0.0, 2.0, 0.01)
-
-# fig, axs = plt.subplots(2, 1, constrained_layout=True)
-# axs[0].plot(t1, f(t1), 'o', t2, f(t2), '-')
-# axs[0].set_title('subplot 1')
-# axs[0].set_xlabel('distance (m)')
-# axs[0].set_ylabel('Damped oscillation')
-# fig.suptitle('This is a somewhat long figure title', fontsize=16)
-
-# axs[1].plot(t3, np.cos(2*np.pi*t3), '--')
-# axs[1].set_xlabel('time (s)')
-# axs[1].set_title('subplot 2')
-# axs[1].set_ylabel('Undamped')
-
-# plt.show()
