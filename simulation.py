f4b6033bbf19320774d4ab2470ec422f33c8a612
deepadmax <maximillian.strand@gmail.com>
2021-01-07 02:32:51 +0100

    added iteration of simulation

--- simulation.py
+++ simulation.py
@@ -1,41 +1,78 @@
 from controller import Controller
 
 
+FAILSAFE_DURATION = 1000
+
+
 class Simulation:
-    def __init__(self, controller, targets, keys=None):
-        """ Run a simulation on a Controller object """
-        
-        self.controller = controller
-        self.targets = targets
+	def __init__(self, controller, targets, timesteps=1):
+		""" Run a simulation on a Controller object
+		
+		targets is a list of tuples each containing length & altitude
+		"""
+		
+		self.controller = controller
+		self.targets = [None, *targets]
+		self.timesteps = timesteps
 
-        # Keys to log
-        self.keys = keys
-        self.data = {}
+	def __iter__(self):
+		self.next_target()
+		
+		i = 1
+		while True:
+			target, length = self.targets[0]
 
-    def __iter__(self):
-        for i in range(0, duration * timesteps, 1):
-            self.controller.update()
+			distance = abs(self.controller.rocket.altitude - target)
+			if i > length and distance <= 50:
+				self.next_target()
+				print(f'Target of {target} is met.')
+
+			if self.targets and i >= FAILSAFE_DURATION:
+				break
+				raise RuntimeError('Simulation failed to meet all targets')
+
+			if not self.targets:
+				break
+
+			yield self.controller.update(target, t=self.timesteps)
+
+			i += 1
+
+	def next_target(self):
+		self.targets.pop(0)
+
+		if self.targets:
+			target, length = self.targets[0]
+			self.controller.set_target(target)
+
+	
 
 
 def example():
-    """ An example test with a rocket launching, hovering, & landing """
-    
-    controller = Controller()
+	""" An example test with a rocket launching, hovering, & landing """
+	
+	controller = Controller()
 
-    r = Rocket(
-        dry_mass=100,
-        altitude=1,
-        fuel=800,
-        thrust=9000,
-        fuel_consumption=10,
-        velocity=0,
-        state='hover',
-        target_altitude=2000
-    )
+	# targets = [2000, 1500, 2500, 0]
+	targets = [
+		(2000, 100),
+		(3000, 100),
+		(0, 100),
+		(2000, 100)
+	]
+	timesteps = 50
+	frequency = 1
 
-    # targets = [2000, 1500, 2500, 0]
-    targets = [2000, 0]
-    timesteps = 50
-    frequency = 50
+	simulation = Simulation(
+		controller,
+		targets,
+		timesteps
+	)
 
-    yield from Simulation(controller, targets, timesteps)[::frequency]
+	try:
+		for i, data in enumerate(simulation):
+			if i % frequency == 0:
+				yield data
+
+	except RuntimeError as e:
+		print(f'Simulation interupted:\n{e}')
