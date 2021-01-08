decf5284d99c0ecc665270ba0cc56dbaf16d6096
Dimitri Tabatadze <tabatadzedima20@gmail.com>
2021-01-08 14:01:57 +0400

    Revert "Revert "Merge pull request #4 from D-T-666/deepadmax-main""
    This reverts commit f05a1da26afc85a8bfd1d2bf5807e80f5f9d65c9.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 # import matplotlib
 # matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import numpy as np
-import engine
-from functions import *
+import os
+import sys
 import time
 
+import controller
+import simulation
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from functions import *
+
+
 def main():
-	results = engine.run()
+	# Get keys of Rocket through a Dummy Controller object
+	keys = controller.Controller().rocket.dict().keys()
+	results = {key: [] for key in keys}
+	
+	# Start & collect data from example simulation
+	for data in simulation.example():
+		for key, value in data.items():
+			results[key].append(value)
+
 
 	plt.style.use('dark_background')
 	fig = plt.figure(figsize=(12, 16))
@@ -19,23 +34,23 @@ def main():
 	axs[4] = fig.add_subplot(3, 2, 5)
 	axs[5] = fig.add_subplot(3, 2, 4)
 
-	axs[0].plot(results["alt"])
-	axs[0].plot([a-e for a,e in zip(results["alt"], results["ed"])], '--', alpha=0.3)
-	axs[0].plot([0 for a in results["alt"]], '-.')
+	axs[0].plot(results["altitude"])
+	axs[0].plot([a-e for a,e in zip(results["altitude"], results["estimated_distance"])], '--', alpha=0.3)
+	axs[0].plot([0 for a in results["altitude"]], '-.')
 	axs[0].set_title('altitude')
 
-	axs[1].plot(results["vel"])
-	axs[1].plot([0 for a in results["alt"]], '-.')
+	axs[1].plot(results["velocity"])
+	axs[1].plot([0 for a in results["altitude"]], '-.')
 	axs[1].set_title('velocity')
 
 	axs[2].plot(results["fuel"])
 	axs[2].set_title('fuel')
 
-	axs[3].plot(results["ed"])
-	axs[3].set_title('estimated distance')
+	axs[3].plot(results["estimated_distance"])
+	axs[3].set_title('estimated_distance')
 
-	axs[4].plot(results["acc"])
-	axs[4].plot([0 for a in results["alt"]], alpha=0.5)
+	axs[4].plot(results["acceleration"])
+	axs[4].plot([0 for a in results["altitude"]], alpha=0.5)
 	axs[4].set_title('acceleration')
 
 	axs[5].plot(results["throttle"])
@@ -43,17 +58,18 @@ def main():
 
 	fig.tight_layout()
 
-	# plt.savefig('D:\\python\\rocket-sim\plots\\fig-'+str(int(time.time()))+'.svg')
-	plt.show()
 
-main()
+	# Save figure to plots folder
+	local_directory = os.path.dirname(sys.argv[0])
+	plots_directory = os.path.join(local_directory, 'plots')
+	file_name = f'fig-{int(time.time())}.svg'
+	file_path = os.path.join(plots_directory, file_name)
 
-# def plot():
-#     plt.style.use('dark_background')
-#     fig, axs = plt.subplots(2, 2, constrained_layout=True)
+	if not os.path.exists(plots_directory):
+		os.makedirs(plots_directory)
 
-# 	axs[0][0] = 0
-	
-#     plt.show(block=True)
+	plt.savefig(file_path)
 
-# plot()
+
+if __name__ == '__main__':
+	main()
--- pid.py
+++ pid.py
@@ -1,13 +1,42 @@
 class PID:
-	def __init__(self):
+	def __init__(self, a, b, c, initial_error=None):
+		# (a, b, c) are the weights for (p, i, d)
+		self.a = a
+		self.b = b
+		self.c = c
+
+		# Proportional, Integral, & Derivative
 		self.p = 0
 		self.i = 0
 		self.d = 0
 
-	def update(self, target_alt, alt, vel, acc, time_steps):
-		self.p = 0.4 * (target_alt - alt)
-		self.i += 0.001 * (target_alt - alt) / time_steps
-		self.d = -2 * vel
-	
-	def get_value(self):
+		# Provided an initial error, self.prepare isn't necessary
+		if initial_error is not None:
+			self.last_error = initial_error
+
+	def __call__(self, *args):
+		self.update(*args)
+		return self.output()
+		
+	def prepare(self, current, target):
+		""" Run at start to give the controller an initial error
+			from which to calculate the first derivative
+		"""
+		self.last_error = current - target
+
+	def update(self, error, derivative=None, t=1):
+		if derivative is None:
+			# Calculate derivate using the current & previous errors
+			try:
+				derivative = error - self.last_error
+			except AttributeError:
+				raise RuntimeError(
+					f'Controller must be initialized properly, with {type(self).__name__}.prepare')
+
+		# Calculate the PID
+		self.p = self.a * error
+		self.i += self.b * error / t
+		self.d = self.c * derivative
+
+	def output(self):
 		return self.p + self.i + self.d
